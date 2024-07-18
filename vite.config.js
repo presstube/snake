05d854d53f252a8c19c9924c0d26e8445b139910
presstube <james.presstube@gmail.com>
2024-07-18 11:58:17 -0400

    changed build base

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "./",
   build: {
     outDir: "docs", // Replace with your desired output directory
-    base: "./",
   },
 });
